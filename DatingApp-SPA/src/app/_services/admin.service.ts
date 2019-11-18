98590107f5fbbda6d3dccac76c05d4c4a55b3b2c
Kevin Wong <kevin.wong1@gmail.com>
2019-11-17 21:12:51 -0500

    Sec21:216-Pass data outOfModal & hook up to API

--- DatingApp-SPA/src/app/_services/admin.service.ts
+++ DatingApp-SPA/src/app/_services/admin.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../_models/user';
 
 
 @Injectable({
@@ -13,6 +14,10 @@ export class AdminService {
 
   getUsersWithRoles() {
     return this.http.get(this.baseUrl + 'admin/usersWithRoles');
+  }
+
+  updateUserRoles(user: User, roles: {}) {
+    return this.http.post(this.baseUrl + 'admin/editRoles/' + user.userName, roles);
   }
 
 }
--- DatingApp-SPA/src/app/admin/user-management/user-management.component.ts
+++ DatingApp-SPA/src/app/admin/user-management/user-management.component.ts
@@ -34,7 +34,18 @@ export class UserManagementComponent implements OnInit {
 
     };
     this.bsModalRef = this.modalService.show(RolesModalComponent, { initialState });
-    this.bsModalRef.content.closeBtnName = 'Close';
+    this.bsModalRef.content.updateSelectedRoles.subscribe((values) => {
+      const rolesToUpdate = {
+        roleNames: [...values.filter(el => el.checked === true).map(el => el.name)]
+      };
+      if (rolesToUpdate) {
+        this.adminService.updateUserRoles(user, rolesToUpdate).subscribe(() => {
+          user.roles = [...rolesToUpdate.roleNames];
+        }, error => {
+          console.log(error);
+        });
+      }
+    });
   }
 
   private getRolesArray(user) {
@@ -50,6 +61,7 @@ export class UserManagementComponent implements OnInit {
     for (let i = 0; i < availableRoles.length; i++) {
       let isMatch = false;
       for (let j = 0; j < userRoles.length; j++) {
+
         if (availableRoles[i].name === userRoles[j]) {
           isMatch = true;
           availableRoles[i].checked = true;
